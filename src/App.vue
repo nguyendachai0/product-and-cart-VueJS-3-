e25e5dbdeb90d82023da8cc2b99ac5ef08e371cd
Nguyen Dac Hai <nguyendmim@gmail.com>
2023-10-08 13:44:56 +0700

    first commit 8-10-2023

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <div @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>{{totalQuantity}}</span>
+    </div>
+  </header>
+  <router-view :inventory="inventory" :addToCart="addToCart" />
+
+  <Sidebar  v-if="showSidebar" :toggle="toggleSidebar" :cart="cart" :inventory="inventory" :remove="removeItem"/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import food from './food.json'
+import Sidebar from './components/Sidebar.vue'
 
-nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    Sidebar
+  },
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data () {
+    return {
+      showSidebar: false,
+      inventory: food,
+      cart: {}
 
-nav a.router-link-exact-active {
-  color: #42b983;
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
+    }
+  },
+  methods: {
+    addToCart (name, quantity) {
+      if (!this.cart[name]) this.cart[name] = 0
+
+      this.cart[name] += quantity
+    },
+    toggleSidebar () {
+      this.showSidebar = !this.showSidebar
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    }
+  }
 }
-</style>
+</script>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,22 @@
+<template>
+    <main class="wrapper">
+        <h1>Products</h1>
+        <div class="card-container">
+          <ProductCard v-for="(product, index) in inventory.slice(0,6)" :key="product.id" :index="index" class="card" :product="product" :addToCart="addToCart">
+            <button @click="addToCart(product.name, i)" class="btn btn-light">
+              Add to cart
+            </button>
+          </ProductCard>
+        </div>
+      </main>
+</template>
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
